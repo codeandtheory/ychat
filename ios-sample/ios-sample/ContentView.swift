7d817156b6500330236d5d4798f66b8bad0d6bf2
Koji Osugi <koji097@gmail.com>
2023-01-20 22:37:38 -0300

    Remove chat gpt from ios sample

--- ios-sample/ios-sample/ContentView.swift
+++ ios-sample/ios-sample/ContentView.swift
@@ -1,17 +1,6 @@
 import SwiftUI
-import chat_gpt
-
-
 
 struct ContentView: View {
-    
-    init() {
-        let chat = ChatGptCompanion.shared.create(apiKey: "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")
-        Task.init {
-            let result = try! await chat.completion(input: "")
-            let a = result
-        }
-    }
     
     var body: some View {
         Text("Hello world!")
