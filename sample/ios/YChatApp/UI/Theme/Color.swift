9a6ff4d6f3f38a7a05fa0a0cb812f6780be4960d
Koji Osugi <koji097@gmail.com>
2023-04-10 10:32:44 -0300

    refactor: change colors

--- sample/ios/YChatApp/UI/Theme/Color.swift
+++ sample/ios/YChatApp/UI/Theme/Color.swift
@@ -9,52 +9,64 @@
 import SwiftUI
 
 extension Color {
+    static var text1: Color {
+        Color("Text1")
+    }
+    
+    static var text2: Color {
+        Color("Text2")
+    }
+    
+    static var text3: Color {
+        Color("Text3")
+    }
+    
+    static var text4: Color {
+        Color("Text4")
+    }
+    
     static var background: Color {
-        Color.white
+        Color("Background")
     }
-
-    static var accentMain: Color {
-        Color("AccentMain")
+    
+    static var icon: Color {
+        Color("Icon")
     }
-
-    static var primaryDark: Color {
-        Color("PrimaryDark")
+    
+    static var divider: Color {
+        Color("Divider")
     }
-
-    static var primaryMain: Color {
-        Color("PrimaryMain")
+    
+    static var accent: Color {
+        Color("Accent")
     }
-
-    static var primaryMedium: Color {
-        Color("PrimaryMedium")
+    
+    static var onAccent: Color {
+        Color("OnAccent")
     }
-
-    static var primaryLight: Color {
-        Color("PrimaryLight")
+    
+    static var accentLight: Color {
+        Color("AccentLight")
     }
-
-    static var primaryExtraLight: Color {
-        Color("PrimaryExtraLight")
+    
+    static var primary1: Color {
+        Color("Primary1")
     }
-
-    static var grayDark: Color {
-        Color("GrayDark")
+    
+    static var primary2: Color {
+        Color("Primary2")
     }
-
-    static var grayMain: Color {
-        Color("GrayMain")
+    
+    static var primary3: Color {
+        Color("Primary3")
     }
-
-    static var grayMedium: Color {
-        Color("GrayMedium")
+    
+    static var primary4: Color {
+        Color("Primary4")
     }
-
-    static var grayLight: Color {
-        Color("GrayLight")
-    }
-
-    static var grayExtraLight: Color {
-        Color("GrayExtraLight")
+    
+    static var primary5: Color {
+        Color("Primary5")
     }
 }
 
@@ -62,56 +74,84 @@ private struct ColorSample: View {
     var body: some View {
         ScrollView {
             VStack(spacing: 0) {
+                textColors()
+                otherColors()
+                accentColors()
                 primaryColors()
-                grayColors()
             }
         }
     }
-
-    private func primaryColors() -> some View {
+    
+    private func textColors() -> some View {
         Group {
-            Text("accentMain")
-                .applyStyle(Color.accentMain)
-            Text("primaryDark")
-                .applyStyle(Color.primaryDark)
-            Text("primaryMain")
-                .applyStyle(Color.primaryMain)
-            Text("primaryMedium")
-                .applyStyle(Color.primaryMedium)
-            Text("primaryLight")
-                .applyStyle(Color.primaryLight)
-            Text("primaryExtraLight")
-                .applyStyle(Color.primaryExtraLight)
+            Text("text1")
+                .applyStyle(.text1, .onAccent)
+            Text("text2")
+                .applyStyle(.text2, .onAccent)
+            Text("text3")
+                .applyStyle(.text3, .onAccent)
+            Text("text4")
+                .applyStyle(.text4, .accent)
         }
     }
-
-    private func grayColors() -> some View {
+    
+    private func otherColors() -> some View {
         Group {
-            Text("grayDark")
-                .applyStyle(Color.grayDark)
-            Text("grayMain")
-                .applyStyle(Color.grayMain)
-            Text("grayMedium")
-                .applyStyle(Color.grayMedium)
-            Text("grayLight")
-                .applyStyle(Color.grayLight)
-            Text("grayExtraLight")
-                .applyStyle(Color.grayExtraLight)
+            Text("background")
+                .applyStyle(.background, .accent)
+            Text("icon")
+                .applyStyle(.icon, .onAccent)
+            Text("divider")
+                .applyStyle(.divider, .accent)
+        }
+    }
+    
+    private func accentColors() -> some View {
+        Group {
+            Text("accent")
+                .applyStyle(.accent, .onAccent)
+            Text("onAccent")
+                .applyStyle(.onAccent, .accent)
+            Text("accentLight")
+                .applyStyle(.accentLight, .accent)
+        }
+    }
+    
+    private func primaryColors() -> some View {
+        Group {
+            Text("primary1")
+                .applyStyle(.primary1, .onAccent)
+            Text("primary2")
+                .applyStyle(.primary2, .onAccent)
+            Text("primary3")
+                .applyStyle(.primary3, .accent)
+            Text("primary4")
+                .applyStyle(.primary4, .accent)
+            Text("primary5")
+                .applyStyle(.primary5, .accent)
         }
     }
 }
 
 private extension Text {
-    func applyStyle(_ color: Color) -> some View {
+    func applyStyle(_ background: Color, _ foregroundColor: Color) -> some View {
         self.frame(maxWidth: .infinity)
-        .padding()
-        .border(.black, width: 0.5)
-        .background(color)
+            .padding()
+            .border(.black, width: 0.5)
+            .background(background)
+            .foregroundColor(foregroundColor)
     }
 }
 
-struct ColorSample_Previews: PreviewProvider {
+struct ColorLightSample_Previews: PreviewProvider {
     static var previews: some View {
         ColorSample()
+    }
+}
+
+struct ColorDarkSample_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorSample()
+            .preferredColorScheme(.dark)
     }
 }
