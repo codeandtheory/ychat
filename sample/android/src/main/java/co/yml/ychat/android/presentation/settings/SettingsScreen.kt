96fa304ff826c3e4f4e0bac83fa765396f71b4a4
Samir Antonio <samir.ma@gmail.com>
2023-04-25 17:00:18 +0100

    SettingsScreen

--- sample/android/src/main/java/co/yml/ychat/android/presentation/settings/SettingsScreen.kt
+++ sample/android/src/main/java/co/yml/ychat/android/presentation/settings/SettingsScreen.kt
@@ -3,25 +3,59 @@ package co.yml.ychat.android.presentation.settings
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.selection.selectable
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.RadioButton
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import co.yml.ychat.android.presentation.models.ModelsScreen
-import co.yml.ychat.android.ui.components.feedback.Feedback
-import co.yml.ychat.android.ui.components.feedback.model.FeedbackState
-import co.yml.ychat.android.ui.theme.TypographyStyle
+import androidx.compose.ui.unit.dp
+import co.yml.ychat.android.presentation.models.viewmodel.ModelsViewModel
 import co.yml.ychat.android.ui.theme.YChatTheme
+import org.koin.androidx.compose.getViewModel
 
 @Composable
-internal fun SettingsScreen() {
+internal fun SettingsScreen(viewModel: SettingsScreenViewModel = getViewModel()) {
+    val providers = listOf("ChatGPT", "Custom")
+    var selectedProvider = remember { mutableStateOf(providers[0]) }
+
     Column(
         modifier = Modifier
             .background(YChatTheme.colors.background)
-            .fillMaxHeight(),
-        verticalArrangement = Arrangement.Center,
+            .fillMaxHeight()
+            .padding(4.dp),
+        verticalArrangement = Arrangement.Top,
     ) {
-        Feedback(feedbackState = FeedbackState.CONSTRUCTION)
+        Text("Select Provider:")
+        providers.forEach { provider ->
+            Row(
+                Modifier
+                    .fillMaxWidth()
+                    .selectable(
+                        selected = (provider == selectedProvider.value),
+                        onClick = { selectedProvider.value = provider }
+                    ),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                RadioButton(
+                    selected = (provider == selectedProvider.value),
+                    onClick = { selectedProvider.value = provider }
+                )
+                Text(
+                    text = provider,
+                    style = MaterialTheme.typography.body1.merge(),
+                    modifier = Modifier.padding(start = 0.dp)
+                )
+            }
+        }
     }
 }
 
